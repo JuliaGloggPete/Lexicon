e55803e3f1689a4d93d9597075e36381b6dee8f2
Julia Petersson <julia.gloggnitzer@outlook.com>
2022-10-14 13:02:09 +0200

    added one Item to the list

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String,String[]> dictonary = new HashMap;
+dictonary.put("Hello", new String[]{"Hej", "Hi "});
+
+
+
+
+
+
+
+        System.out.println("Hello world!");
+    }
+}
